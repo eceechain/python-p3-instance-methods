e57eeeb4c19ffdee71ff410e1a594e24121ccc3b
ecee <you@example.com>
2023-11-25 21:59:41 +0300

    Done with Asignment

--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,12 @@
 
 class Person:
     # Class body goes here
+    #Instance method definition
+    def talk(self):
+        print("Hello World!")
 
     #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+
+
